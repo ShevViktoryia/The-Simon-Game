0885175d4d762d969510a2a9b4dad114d4dd9dc0
Victoria <shevvictoryia@gmail.com>
2024-09-05 23:35:05 +0300

    feat: add checkAnswer functionality

--- script.js
+++ script.js
@@ -11,8 +11,26 @@ const nextSequence = () => {
   gameChoise.push(randomColor);
 };
 
+const checkAnswer = () => {
+  if (gameChoise[gameChoise.length - 1] === userChoise[userChoise.length - 1]) {
+    if (gameChoise.length === userChoise.length) {
+      setTimeout(() => {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    $("body").addClass("game-over");
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+  }
+};
+
 $(".btn").click(function () {
   userChoise.push($(this).attr("id"));
+  checkAnswer();
   console.log(userChoise);
 });
 nextSequence();
